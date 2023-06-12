69be8321a0885fd902bbd1895685c4a81dcaab8f
Roman <romchikus1987@gmail.com>
2023-06-12 13:20:56 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,176 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newBlog, setNewBlog] = useState({ title: '', author: '', url: '' })
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+  const [successMessage, setSuccessMessage] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+    blogService.getAll().then((blogs) => setBlogs(blogs))
   }, [])
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+
+      setUser(user)
+      blogService.setToken(user.token)
+
+      const tokenExpirationTime = new Date(user.expirationTime)
+
+      if (tokenExpirationTime < new Date()) {
+        setUser(null)
+        window.localStorage.removeItem('loggedBlogappUser')
+        setErrorMessage('Session expired. Please log in again.')
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+      }
+    }
+  }, [])
+
+  const addBlog = (event) => {
+    event.preventDefault()
+    console.log(newBlog)
+    const blogObject = {
+      title: newBlog.title,
+      author: newBlog.author,
+      url: newBlog.url,
+    }
+    if (!blogObject.title || !blogObject.author || !blogObject.url) {
+      alert('Please fill in all info')
+    }
+    blogService.create(blogObject).then((returnedBlog) => {
+      setBlogs(blogs.concat(returnedBlog))
+      setSuccessMessage(
+        `a new blog ${returnedBlog.title} by ${returnedBlog.author} added!`
+      )
+      setTimeout(() => {
+        setSuccessMessage(null)
+      }, 5000)
+      setNewBlog({ title: '', author: '', url: '' })
+    })
+  }
+
+  const handleBlogChange = (event, property) => {
+    setNewBlog({ ...newBlog, [property]: event.target.value })
+  }
+  const handleLogin = async (e) => {
+    e.preventDefault()
+    try {
+      const user = await loginService.login({ username, password })
+      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      setSuccessMessage(`Hello ${user.name}👋`)
+      setTimeout(() => {
+        setSuccessMessage(null)
+      }, 5000)
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
+  }
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type='text'
+          value={username}
+          name='Username'
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type='password'
+          value={password}
+          name='Password'
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type='submit'>login</button>
+    </form>
+  )
+  const logOut = () => {
+    window.localStorage.clear()
+    setUser(null)
+  }
+
+  const blogForm = () => (
+    <form onSubmit={addBlog} className='blog-form'>
+      <label htmlFor='title'>Title:</label>
+      <input
+        type='text'
+        value={newBlog.title}
+        id='title'
+        className='form-control'
+        onChange={(event) => handleBlogChange(event, 'title')}
+        required
+      />
+
+      <label htmlFor='author'>Author:</label>
+      <input
+        type='text'
+        value={newBlog.author}
+        id='author'
+        className='form-control'
+        onChange={(event) => handleBlogChange(event, 'author')}
+        required
+      />
+
+      <label htmlFor='url'>Url:</label>
+      <input
+        type='text'
+        value={newBlog.url}
+        id='url'
+        className='form-control'
+        onChange={(event) => handleBlogChange(event, 'url')}
+        required
+      />
+
+      <button type='submit'>save</button>
+    </form>
+  )
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      <Notification message={errorMessage} isError={true} />
+      <Notification message={successMessage} />
+      {!user && (
+        <>
+          <h3>Log in to an application</h3>
+          {loginForm()}
+        </>
+      )}
+      {user && (
+        <div>
+          <h2>Blogs</h2>
+          {user.name} logged in
+          <button type='submit' style={{ marginLeft: '5px' }} onClick={logOut}>
+            log out
+          </button>
+          <p>Create new</p>
+          {blogForm()}
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </div>
       )}
     </div>
   )
