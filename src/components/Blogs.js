37b262d35fc669d142f219a52bb93eb36ce78292
Roman <romchikus1987@gmail.com>
2023-07-06 17:54:22 +0300

    updated

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -129,7 +129,7 @@ const Blogs = ({ setNotification, setErrorMessage }) => {
 
 
   return (
-    <ul>
+    <>
       {blogs.map(blog =>
         <Blog
           key={blog.id}
@@ -140,7 +140,8 @@ const Blogs = ({ setNotification, setErrorMessage }) => {
           delBlog={() => delBlog(blog.id)}
         />
       )}
-    </ul>)
+    </>
+  )
 }
 
 
