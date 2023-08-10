d3d561f73ded15d40510eebffcba68b7fd82f533
Roman <romchikus1987@gmail.com>
2023-08-10 20:33:04 +0300

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import { useEffect, useRef } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-
-/* test */
 import Blogs from './components/Blogs'
 import blogService from './services/blogs'
 import Notification from './components/Notification'
